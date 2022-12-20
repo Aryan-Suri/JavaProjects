ee1d3657ec62b4b3ab4dd79b254f65a2c99d0b09
Aryan-Suri <103606432+Aryan-Suri@users.noreply.github.com>
2022-12-19 22:02:24 -0500

    Create FamilyTree.java

---
+++ FamilyTree/FamilyTree.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+/**
+ * This class creates a family tree object that will be used later to create a general tree
+ *
+ */
+public class FamilyTree 
+{
+    //instance variables
+    public Person header;
+    public int size;  
+    
+    public FamilyTree() 
+    {
+        
+    }
+    //getter method
+    public int getSize()
+    {
+        return size;
+    }
+}
